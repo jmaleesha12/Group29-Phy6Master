92738a424c6d52faba352e6c2b0b25b727afc9fc
sehanya <it24101643@my.sliit.lk>
2026-02-17 22:47:26 +0530

    Initial project structure

---
+++ src/test/java/com/example/Phy6_Master/Phy6MasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Phy6_Master;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Phy6MasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
