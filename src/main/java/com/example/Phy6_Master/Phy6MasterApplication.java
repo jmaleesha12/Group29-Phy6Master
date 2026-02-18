a8469af553ac961cdcc30937c5707f16d6136317
MenathDulsana <it24101846@my.sliit.lk>
2026-02-18 19:21:10 +0530

    project structure

---
+++ src/main/java/com/example/Phy6_Master/Phy6MasterApplication.java
@@ -0,0 +1,13 @@
+package com.example.Phy6_Master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Phy6MasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Phy6MasterApplication.class, args);
+	}
+
+}
