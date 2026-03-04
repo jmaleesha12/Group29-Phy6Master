1dc19d567db4b18ecb8df4b88394ec5128c3349a
Dayan-01 <dayanherath8970@gmail.com>
2026-03-04 20:55:47 +0530

    Changes into the student dashboard backend

--- src/main/java/com/example/Phy6_Master/repository/ChatConversationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Phy6_Master.repository;
-
-import com.example.Phy6_Master.model.ChatConversation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatConversationRepository extends JpaRepository<ChatConversation, Long> {
-}
